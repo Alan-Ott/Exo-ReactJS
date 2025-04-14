31bec107da2e30c5c11c0ea4d8104abc455991e1
Alan-Ott <alan.ott@ecole-hexagone.com>
2025-04-14 12:14:49 +0200

    shuffle

--- app-number/src/App.jsx
+++ app-number/src/App.jsx
@@ -12,29 +12,33 @@ function App() {
   const [state, dispatch] = useReducer(reducer, initialState);
 
   const randomNumberInRange = (min, max) => {
+
     return Math.floor(Math.random() * (max - min + 1)) + min;
   };
 
   const addRandNum = () => {
 
-    dispatch({type: 'ADD_RANDNUM', payload: randomNumberInRange(1, 10)})
+    dispatch({ type: 'ADD', payload: randomNumberInRange(1, 10) });
   }
 
+  const shuffleRandNum = () => {
 
+    dispatch({ type: 'SHUFFLE' });
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank"><img src={viteLogo} className="logo" alt="Vite logo" /></a>
-        <a href="https://react.dev" target="_blank"><img src={reactLogo} className="logo react" alt="React logo" /></a>
-      </div>
-      <h1>Vite + React</h1>
+      <h1>Random Numbers</h1>
 
       <div className="card">
 
         <div className='card-title'>
-          <button onClick={ addRandNum}>
+          <button onClick={addRandNum}>
             Add Random Number
+          </button>
+
+          <button onClick={shuffleRandNum}>
+            Shuffle List
           </button>
         </div>
 
--- app-number/src/store/randnumlist.jsx
+++ app-number/src/store/randnumlist.jsx
@@ -4,11 +4,17 @@ export const initialState = {
 
 export function reducer(state, action) {
     switch (action.type) {
-        case 'ADD_RANDNUM':
+        case 'ADD':
 
             const randNums = [...state.randNums, {num : action.payload}];
 
             return { ...state, randNums : randNums};
+
+        case 'SHUFFLE':
+
+            const shuffledNums = [...state.randNums.sort((a, b) => 0.5 - Math.random()) ];
+
+            return { ...state, randNums : shuffledNums};
 
         default:
             return state;
